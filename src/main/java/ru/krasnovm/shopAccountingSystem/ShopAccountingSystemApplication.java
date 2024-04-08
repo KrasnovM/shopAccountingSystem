c9e7b908189c4f937e056a2e10110b0605cd7ad7
KrasnovM <krasnov.m2000@gmail.com>
2024-04-08 20:17:21 +0300

    Initial commit

---
+++ src/main/java/ru/krasnovm/shopAccountingSystem/ShopAccountingSystemApplication.java
@@ -0,0 +1,13 @@
+package ru.krasnovm.shopAccountingSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopAccountingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopAccountingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/krasnovm/shopAccountingSystem/ShopAccountingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.krasnovm.shopAccountingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopAccountingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
