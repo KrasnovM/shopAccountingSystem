411cdcc1783caf3797e06546422e2ac18d6e666f
KrasnovM <krasnov.m2000@gmail.com>
2024-04-08 21:04:08 +0300

    changed CategoryService, made categories modifiable

--- src/main/java/ru/krasnovm/shopAccountingSystem/service/CategoryService.java
+++ src/main/java/ru/krasnovm/shopAccountingSystem/service/CategoryService.java
@@ -2,6 +2,7 @@ package ru.krasnovm.shopAccountingSystem.service;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import ru.krasnovm.shopAccountingSystem.dto.CategoryDTO;
 import ru.krasnovm.shopAccountingSystem.entity.Category;
 import ru.krasnovm.shopAccountingSystem.repository.CategoryRepository;
 
@@ -13,6 +14,12 @@ public class CategoryService {
 
     private final CategoryRepository categoryRepository;
 
+    public Category create(CategoryDTO dto) {
+        return categoryRepository.save(Category.builder()
+                .name(dto.getName())
+                .build());
+    }
+
     public List<Category> readAll() {
         return categoryRepository.findAll();
     }
@@ -20,5 +27,13 @@ public class CategoryService {
     public Category readById(Long id) {
         return categoryRepository.findById(id).orElseThrow(() ->
                 new RuntimeException("Category not found: " + id));
+    }
+
+    public Category update(Category category) {
+        return categoryRepository.save(category);
+    }
+
+    public void delete(Long id) {
+        categoryRepository.deleteById(id);
     }
 }
