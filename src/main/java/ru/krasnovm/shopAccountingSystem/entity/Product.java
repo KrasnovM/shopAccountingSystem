657ac8dc6d048adbafdc438442dabd2736e7d010
KrasnovM <krasnov.m2000@gmail.com>
2024-04-08 20:44:40 +0300

    added product class

---
+++ src/main/java/ru/krasnovm/shopAccountingSystem/entity/Product.java
@@ -0,0 +1,25 @@
+package ru.krasnovm.shopAccountingSystem.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    private String name;
+    private Double price;
+    private Short amount;
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+}
