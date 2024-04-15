2eae37b516f07020ebab7010197b6ebf347ead5b
KrasnovM <krasnov.m2000@gmail.com>
2024-04-15 13:27:39 +0300

    created SHopUser entity

---
+++ src/main/java/ru/krasnovm/shopAccountingSystem/entity/ShopUser.java
@@ -0,0 +1,21 @@
+package ru.krasnovm.shopAccountingSystem.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class ShopUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
+    private String name;
+    private String password;
+    private String roles;
+}
